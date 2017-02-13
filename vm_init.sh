dcda4a703651b5eb1384dbb055fe4b8a314ddff2
William Yeung <william@tofugear.com>
2017-02-14 00:36:41 +0800

    updated script to contain better ssh setup

--- vm_init.sh
+++ vm_init.sh
@@ -2,11 +2,51 @@
 
 function usage
 {
-    echo "Usage: vm_init -g git_url -p private_key"
+    echo "Usage: vm_init -g git_url -p private_key -gh git_host -gu git_user"
 }
+
+function setup_sshkey 
+{
+    mkdir -p ~/.ssh
+    private_key=$(echo $private_key | base64 --decode); 
+    keyfile=~/.ssh/$git_host.key
+    echo $private_key > $keyfile
+
+    [ -f ~/.ssh/config ] && rm ~/.ssh/config
+
+cat >> ~/.ssh/config << EOF
+Host $git_host
+HostName $git_host
+User $git_user
+IdentityFile $keyfile
+EOF
+
+    chmod 700 ~/.ssh 
+    chmod 400 ~/.ssh/*
+    eval `ssh-agent`
+    ssh-add
+
+}
+
+function execute
+{
+    setup_sshkey
+    mkdir -p /var/deploy 
+
+    git clone $git_url /var/deploy 
+
+    sh /var/deploy/vm_deploy.sh
+}
+
 
 while [ "$1" != "" ]; do
     case $1 in
+        -gh | --git-host )      shift
+                                git_host=$1
+                                ;;
+        -gu | --git-user )      shift   
+                                git_user=$1
+                                ;;
         -g | --git-url )        shift
                                 git_url=$1
                                 ;;
@@ -22,22 +62,9 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [ -z "$git_url" ] || [ -z "$private_key" ]; then
+if [ -z "$git_url" ] || [ -z "$private_key" ] || [ -z "$git_user" ] || [ -z "$git_host" ]; then
     usage    
 else
     execute
 fi
 
-
-function execute
-{
-    mkdir -p ~/.ssh
-    echo $private_key > ~/.ssh/deploy_id_ed25519
-    chmod -R 700 ~/.ssh 
-    ssh-add ~/.ssh/deploy_id_ed25519
-    mkdir -p /var/deploy 
-
-    git clone $git_url /var/deploy 
-
-    source /var/deploy/vm_deploy.sh
-}
